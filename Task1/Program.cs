03191c77211f07f8ec78c7bf950a7671a6569fe3
zamotin-aa <zamotin.info@gmail.com>
2023-10-03 12:35:07 +0300

    Main

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,34 +3,54 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
 
-// Метод для фильтрации массива по заданному условию
-static string[] FilterArrayByLength(string[] inputArray, int maxLength)
+using System;
+
+class Program
 {
-    int count = 0;
-
-    // Считаем количество строк, удовлетворяющих условию
-    for (int i = 0; i < inputArray.Length; i++)
+    static void Main()
     {
-        if (inputArray[i].Length <= maxLength)
+        Console.WriteLine("Введите строки через запятую:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        string[] resultArray = FilterArrayByLength(inputArray, 3);
+
+        Console.WriteLine("Результат:");
+        foreach (string str in resultArray)
         {
-            count++;
+            Console.WriteLine(str);
         }
     }
 
-    // Создаем новый массив нужной длины
-    string[] resultArray = new string[count];
-    int resultIndex = 0;
-
-    // Заполняем новый массив подходящими строками
-    for (int i = 0; i < inputArray.Length; i++)
+    // Метод для фильтрации массива по заданному условию
+    static string[] FilterArrayByLength(string[] inputArray, int maxLength)
     {
-        if (inputArray[i].Length <= maxLength)
+        int count = 0;
+
+        // Считаем количество строк, удовлетворяющих условию
+        for (int i = 0; i < inputArray.Length; i++)
         {
-            resultArray[resultIndex] = inputArray[i];
-            resultIndex++;
+            if (inputArray[i].Length <= maxLength)
+            {
+                count++;
+            }
         }
+
+        // Создаем новый массив нужной длины
+        string[] resultArray = new string[count];
+        int resultIndex = 0;
+
+        // Заполняем новый массив подходящими строками
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= maxLength)
+            {
+                resultArray[resultIndex] = inputArray[i];
+                resultIndex++;
+            }
+        }
+
+        return resultArray;
     }
 
-    return resultArray;
 }
-
